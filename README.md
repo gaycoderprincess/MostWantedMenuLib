6439975d9490828eb2fd09b4ee226503c3f31eb3
💖 Chloe 💖 <gaycoderprincess@proton.me>
2025-11-25 02:25:04 +0100

    woof :3

---
+++ README.md
@@ -0,0 +1,19 @@
+# Most Wanted Menu Library
+
+A shared library for Need for Speed: Most Wanted in order to have multiple plugins' configurations in one global menu
+
+![preview image](https://i.imgur.com/ktM6DWc.jpeg)
+
+## Installation
+
+- Make sure you have v1.3 of the game, as this is the only version this plugin is compatible with. (exe size of 6029312 bytes)
+- Plop the files into your game folder, press F5 in game to open the menu, any supported mods will be listed.
+- Enjoy, nya~ :3
+
+## Building
+
+Building is done on an Arch Linux system with CLion being used for the build process. 
+
+Before you begin, clone [nya-common](https://github.com/gaycoderprincess/nya-common), [nya-common-nfsmw](https://github.com/gaycoderprincess/nya-common-nfsmw) and [FlatOutUCMenuLib](https://github.com/gaycoderprincess/FlatOutUCMenuLib) to folders next to this one, so they can be found.
+
+You should be able to build the project now in CLion.
---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <windows.h>
+#include <d3d9.h>
+#include <mutex>
+#include <toml++/toml.hpp>
+
+#include "nya_dx9_hookbase.h"
+#include "nya_commonhooklib.h"
+#include "nfsmw.h"
+
+#include "include/chloemenulib.h"
+
+void DisableKeyboardInput(bool disable) {
+	NyaHooks::bInputsBlocked = disable;
+}
+
+void UpdateD3DProperties() {
+	g_pd3dDevice = *(IDirect3DDevice9**)0x982BDC;
+	ghWnd = *(HWND*)0x982BF4;
+	GetRacingResolution(&nResX, &nResY);
+}
+
+const char* versionString = "NFS Most Wanted Menu Lib 1.00";
+
+#include "menulib.h"
+
+BOOL WINAPI DllMain(HINSTANCE, DWORD fdwReason, LPVOID) {
+	switch( fdwReason ) {
+		case DLL_PROCESS_ATTACH: {
+			if (NyaHookLib::GetEntryPoint() != 0x3C4040) {
+				MessageBoxA(nullptr, "Unsupported game version! Make sure you're using v1.3 (.exe size of 6029312 bytes)", "nya?!~", MB_ICONERROR);
+				return TRUE;
+			}
+
+			InitAndLoadConfig("NFSMWMenuLib_gcp.toml");
+			NyaHooks::PlaceD3DHooks();
+			NyaHooks::aEndSceneFuncs.push_back(D3DHookMain);
+			NyaHooks::aD3DResetFuncs.push_back(OnD3DReset);
+			NyaHooks::PlaceWndProcHook();
+			NyaHooks::aWndProcFuncs.push_back(WndProcHook);
+			NyaHooks::PlaceInputBlockerHook();
+		} break;
+		default:
+			break;
+	}
+	return TRUE;
+}
